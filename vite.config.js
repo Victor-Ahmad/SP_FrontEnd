e0bc1762713f58dc3652a4c53fae251a1f68793b
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-08-18 19:08:58 +0300

    vimport path from "path";

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
